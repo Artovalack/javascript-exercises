f197c1cc82d79f4ee6fe985b94bdce10af16867f
Artovalack <279592365+Artovalack@users.noreply.github.com>
2026-08-07 17:58:35 +0800

    Submitted 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, item) {
+    removedItem = arr.indexOf(item);
+    arr.splice(removedItem, 1);
+    return arr;
 };
 
 // Do not edit below this line
