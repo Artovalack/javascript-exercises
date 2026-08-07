cd9537bf80a9044e3e5f688a6fcf5e6b275b09c7
Artovalack <279592365+Artovalack@users.noreply.github.com>
2026-08-07 20:10:27 +0800

    Submitted 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  cel = ((far-32) * 5/9);
+  return parseFloat(cel.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(150.8));
+
+const convertToFahrenheit = function(cel) {
+  far = ((cel * 9/5) + 32);
+  return parseFloat(far.toFixed(1));
 };
+
+console.log(convertToFahrenheit(66));
 
 // Do not edit below this line
 module.exports = {
